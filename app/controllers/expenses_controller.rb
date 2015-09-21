b10d30246d51c048c285e8330ccabe6efa79b6ae
German Escobar <german.escobarc@gmail.com>
2015-09-21 16:49:21 -0500

    Implement search concept

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,8 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     if params[:concept]
+      @expenses = @expenses.where("concept like '%#{params[:concept]}%'")
+     end
   end
 end
