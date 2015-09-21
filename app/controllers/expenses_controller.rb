e3badb1f627adbdae521be555f33cfd021eb9023
German Escobar <german.escobarc@gmail.com>
2015-09-21 16:57:40 -0500

    Fix filter by category

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,10 @@
 class ExpensesController < ApplicationController
   def index
     @expenses = Expense.order("date DESC")
-    if params[:concept]
+    if params[:concept].present?
       @expenses = @expenses.where("concept like '%#{params[:concept]}%'")
     end
-    if params[:category_id]
+    if params[:category_id].present?
       @expenses = @expenses.where(category: params[:category_id])
     end
   end
